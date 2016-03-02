e5991998f00818967bf8b1dd4bc16cc5709d35be
Rombi <isu6dsub@gmail.com>
2016-03-02 22:23:13 +0000

    A lot of changes to the code since initial.

--- src/calibrate_motor.py
+++ src/calibrate_motor.py
@@ -12,20 +12,33 @@ pwm = PWM(0x40)
 # Note if you'd like more debug output you can instead run:
 #pwm = PWM(0x40, debug=True)
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+pwm.setPWMFreq(240)                        # Set frequency to 60 Hz
 
-MOTOR_MIN = 1
-MOTOR_END = 3
+MOTOR_LIST = [1,2]
 
 # Change speed of continuous servo on channel O
-raw_input("Unplug the squid and press [Enter].")
-for i in range(MOTOR_MIN, MOTOR_END):
-  pwm.setPWM(i, 0, 4096)
+#raw_input("Unplug the squid and press [Enter].")
+#for i in MOTOR_LIST:
+#  pwm.setPWM(i, 0, 4096)
+pwm.setPWM(1, 0, 0)
+pwm.setPWM(2, 0, 0)
 raw_input("Press [Enter] once the squid is plugged in.")
-time.sleep(2)
-for i in range(MOTOR_MIN, MOTOR_END):
-  pwm.setPWM(i, 0, 385)
-time.sleep(1)
+#time.sleep(2)
+#for i in MOTOR_LIST:
+#  pwm.setPWM(i, 0, 385)
+#time.sleep(1)
+
+#try:
+#  amt = 0
+#  while (amt >= 0 and amt < 4095):
+#    print "Trying", amt
+#    pwm.setPWM(1, 0, int(amt))
+#    time.sleep(0.008)
+#    amt += 1
+#except KeyboardInterrupt:
+#  print "Ending calibration"
+
+#pwm.setPWM(1, 0, 0)
 
 try:
   while (True):
@@ -36,11 +49,11 @@ try:
     motor = int(line[0])
     amt = int(line[1])
 
-    if motor < MOTOR_MIN or motor >= MOTOR_END:
+    if not motor in MOTOR_LIST:
       print "Invalid motor number"
       continue
 
     pwm.setPWM(motor, 0, int(amt))
 except KeyboardInterrupt:
-  pass
+  print("")
 
