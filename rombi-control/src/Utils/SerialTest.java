485024db4d6d200bbe3d09c59dfe00a6cddc3694
Vaughn Dorsey <vwdorsey@iastate.edu>
2016-12-06 16:32:14 -0600

    Switched methods of getting serial input.

--- rombi-control/src/Utils/SerialTest.java
+++ rombi-control/src/Utils/SerialTest.java
@@ -2,6 +2,11 @@ package Utils;
 
 import Sub.Drivers.Serial.PiSerial;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import com.pi4j.io.serial.*;
+
 /**
  * Testing to see what the serial port ends up outputting and if the serial driver actually works.
  * @author vwdorsey
@@ -10,16 +15,33 @@ import Sub.Drivers.Serial.PiSerial;
 public class SerialTest {
 	
 	public static void main(String[] args){
-		PiSerial serial = new PiSerial("/dev/ttyUSB0", 57600, 25);
-		serial.open();
-		while(true) {
-			if(serial.hasData()){
-				if(serial.amountDataAvailable() == 25){
-					String output = serial.read();
-					System.out.println(output);
-				}
+		final Serial serial = SerialFactory.createInstance();
+		serial.addListener(new SerialDataEventListener()
+				{
+					@Override
+					public void dataReceived(SerialDataEvent arg0) {
+						System.out.println("Recieved");
+						
+					}
+				});
+		try {
+			SerialConfig config = new SerialConfig();
+			
+			config.device("/dev/ttyUSB0").baud(Baud._57600);
+			
+			System.out.println("Connecting....");
+			
+			serial.open(config);
+			
+			System.out.println("Connected.");
+			
+			while(true){
+				System.out.println(serial.read(25, Charset.defaultCharset()).toString());
 			}
 		}
-		//serial.close();
+		catch(IOException e){
+			System.out.println("Error.");
+			return;
+		}
 	}
 }
